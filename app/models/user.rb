2bdb43dd69eb07fad9b06bb990407c72cdccb04d
MakerSquare Student <students@themakersquare.com>
2013-12-04 07:29:58 -0600

    Add description column to Users Database and code for full name method in model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
 
-	def full_name 
-    [first_name, last_name].reject(&:blank?).join(" ")
-    puts "#{first_name} and #{last_name}"
+	def full_name    
+    [first_name, last_name].reject(&:blank?).join("     ")
+    # puts "#{first_name} and #{last_name}"
+    
   end
 	
 
 	def to_s
     full_name
-    puts full_name
+   
+    
   end
 end
